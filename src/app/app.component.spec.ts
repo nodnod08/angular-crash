2224025d3ef1a6b2ac5cc1a5b90d8cac29beec10
Dondomie Dungca <dondomiedungca14@gmail.com>
2022-07-29 15:45:47 +0800

    Add errors

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-crash'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('The ultimate angular crash course');
+    expect(app.title).toEqual('The ultimate angular crash course 1');
   });
 
   it('should render title', () => {
